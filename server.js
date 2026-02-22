4601ca62c34f80cdf611c80fc4acb682afb50413
kalai02484 <kalai02484@gmail.com>
2026-02-22 20:22:03 +0530

    express installed and added to code

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 //create a server
- 
+
+//for env
+// Step 1: configure the environment variables
+// Step 2: access the environment variable
+
+//for express
+// Step 1: import the express module
+// Step 2: create an instance of express
+// Step 3: define routes and middleware
+// Step 4: start the server
+
 import http from "http";
-
-// Step 1: for environment variables
 import dotenv from "dotenv";
-// Step 2: configure the environment variables
-dotenv.config();
-// Step 3: access the environment variable
-const port = process.env.PORT || 3000 ;
+import express from "express";
 
+// Create an instance of express
+const app = express();
+// Define a middleware to parse JSON request bodies
+app.use(express.json());
+
+dotenv.config();
+const port = process.env.PORT || 3000 ;
+/*
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-type": "text/plain" });
@@ -18,3 +31,12 @@ http
   .listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+*/
+
+app.get("/", (req, res)=>{
+  res.status(200).send("<h1 style='color: blue;'>Hello World, Welcome to NodeJS</h1>");
+});
+
+app.listen(port, () => {
+  console.log("Server is running");
+});
