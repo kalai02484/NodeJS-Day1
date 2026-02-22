ec1ac25cc5e6ad95db900373a280cb7ad46d4347
kalai02484 <kalai02484@gmail.com>
2026-02-22 19:52:15 +0530

    dotenv installed and configured

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+//create a server
+ 
 import http from "http";
 
-//create a server
-
-const port = 3000;
+// Step 1: for environment variables
+import dotenv from "dotenv";
+// Step 2: configure the environment variables
+dotenv.config();
+// Step 3: access the environment variable
+const port = process.env.PORT || 3000 ;
 
 http
   .createServer((req, res) => {
